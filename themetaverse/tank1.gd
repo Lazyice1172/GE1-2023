734f0884dc7d04e731a2ad3fd6e5a2f68807682a
Lazyice1172 <c20348256@mytudublin.ie>
2023-09-29 16:39:17 +0100

    Tank Game finish

--- themetaverse/tank1.gd
+++ themetaverse/tank1.gd
@@ -1,4 +1,13 @@
 extends CharacterBody3D
 
+@export var move_sp:float = 5
+@export var rt_sp:float = 5
+
 func _process(delta):	
-		translate(Vector3.FORWARD * delta)
+		
+		var move = Input.get_axis("move_forward", "move_backward")
+		var rt = Input.get_axis("rt_left", "rt_right")
+		
+		translate(Vector3.FORWARD * move *move_sp * delta)
+		
+		rotate(Vector3.UP, rt * rt_sp * delta)
